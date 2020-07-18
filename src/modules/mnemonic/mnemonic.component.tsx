ecaaf4171edd818989699de80137da8e3510c83c
Ssen Galanto <galanto@mclinica.com>
2020-07-18 11:04:34 +0800

    chore: create responsive initial page

--- src/modules/mnemonic/mnemonic.component.tsx
+++ src/modules/mnemonic/mnemonic.component.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
 
-import { Section } from 'shared/layouts';
-import { Row } from 'shared/components';
+import { Row, Col, Text } from 'shared/components';
+
+import { S } from './mnemonic.styles';
 
 export const Mnemonic: React.FC = () => (
-  <Section>
-    <Row span={24}>hahaha</Row>
-  </Section>
+  <S.Section>
+    <Row direction="column" alignContent="space-between">
+      <Col>
+        <S.Circle />
+      </Col>
+      <Col flex direction="column" justify="space-between">
+        <Col>
+          <S.Text color="primary" density="medium">
+            Hello,
+          </S.Text>
+          <S.Text color="secondary" density="medium">
+            Welcome to
+          </S.Text>
+          <S.Text color="secondary" density="medium">
+            Mnemonic
+          </S.Text>
+        </Col>
+        <Col flex direction="column" justify="flex-end" alignItems="center">
+          <S.Button>
+            <S.BtnText color="reverse">Login</S.BtnText>
+          </S.Button>
+          <Text >Don&lsquo;t have an account? Sign up</Text>
+        </Col>
+      </Col>
+    </Row>
+  </S.Section>
 );
